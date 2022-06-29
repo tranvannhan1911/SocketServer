a9d1783046a6bafdc23076683eabc1c491cfc900
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-06-29 18:57:20 +0700

    feature open file

--- src/app/Server.java
+++ src/app/Server.java
@@ -7,6 +7,9 @@ import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 
 import app.Server.Handle;
 
@@ -59,14 +62,14 @@ public class Server {
 							out.writeUTF("Failed to restart");
 						}
 
-					} else if (cmd.equals("delete")) {
+					} else if (cmd.equals("open")) {
 						String path = in.readUTF();
-						System.out.println(String.format("> Delete file %s", path));
+						System.out.println(String.format("> Open %s", path));
 
-						if (delete(path)) {
-							out.writeUTF("Deleted the file: " + path);
+						if (open(path)) {
+							out.writeUTF("Opened: " + path);
 						} else {
-							out.writeUTF("Failed to delete the file: " + path);
+							out.writeUTF("Failed to open the file/directory: " + path);
 						}
 					}
 					out.flush();
@@ -110,8 +113,18 @@ public class Server {
 		
 	}
 
-	public boolean delete(String path) {
-		File myObj = new File(path);
-		return myObj.delete();
+	public boolean open(String path) {
+		Desktop desktop = Desktop.getDesktop();
+        File dirToOpen = null;
+        try {
+            dirToOpen = new File(path);
+            desktop.open(dirToOpen);
+            return true;
+        } catch (IllegalArgumentException iae) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+        return false;
 	}
 }
